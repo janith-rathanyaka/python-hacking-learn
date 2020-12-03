f07756f49c3037b4c27e11c71defd8a6454ac123
janith-rathanyaka <72126988+janith-rathanyaka@users.noreply.github.com>
2020-12-03 15:04:48 +0530

    password hacking using python
    sha1,md5,sha256

---
+++ password/scanning/sha1hash.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+from urllib.request import urlopen
+import hashlib
+
+sha1hash = input("[*] Enter sha1 Hash Value: ")
+passlist = str(urlopen('https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10-million-password-list-top-10000.txt').read(), 'utf-8')
+
+for password in passlist.split('\n'):
+     hashguess = hashlib.sha1(bytes(password,'utf-8')).hexdigest()
+     if hashguess == sha1hash:
+            print("[+] The Password is:" + str(password))
+            quit()
+     else:
+            print("[.] Password guess " + str(password) + "does not match")     
+
+
